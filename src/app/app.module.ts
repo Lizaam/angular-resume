002b20759878eded631df026dd9e053511b2c882
Lizaam <lizaame@bumblebee.co.za>
2021-01-05 16:07:36 +0200

    naming convention update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { IntroSectionComponent } from './sections/intro-section/intro-section.component';
+import { IntroComponent } from './sections/intro/intro.component';
 import { LeftNavComponent } from './components/left-nav/left-nav.component';
-import { AboutSectionComponent } from './sections/about-section/about-section.component';
-import { ResumeSectionComponent } from './sections/resume-section/resume-section.component';
-import { ServicesSectionComponent } from './sections/services-section/services-section.component';
-import { WorkSectionComponent } from './sections/work-section/work-section.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ResumeComponent } from './sections/resume/resume.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { WorkComponent } from './sections/work/work.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     SideBarComponent,
-    IntroSectionComponent,
+    IntroComponent,
     LeftNavComponent,
-    AboutSectionComponent,
-    ResumeSectionComponent,
-    ServicesSectionComponent,
-    WorkSectionComponent,
+    AboutComponent,
+    ResumeComponent,
+    ServicesComponent,
+    WorkComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule
--- src/app/sections/resume/resume.component.spec.ts
+++ src/app/sections/resume/resume.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ResumeSectionComponent } from './resume-section.component';
+import { ResumeSectionComponent } from './resume.component';
 
 describe('ResumeSectionComponent', () => {
   let component: ResumeSectionComponent;
