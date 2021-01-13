a01915d218b04a8b8024f3757ae99e3f4c5da716
Lizaam <lizaame@bumblebee.co.za>
2021-01-13 15:41:58 +0200

    added project info component to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/pages/home/home.component';
+import { ProjectInfoComponent } from './pages/project-info/project-info.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'project-info', component: ProjectInfoComponent }
 ];
 
 @NgModule({
