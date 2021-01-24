739e388af164d3ebc740bf3d74443a4a0b595844
Lizaam <lizaame@bumblebee.co.za>
2021-01-24 12:39:04 +0200

    included settings and constant data

--- src/app/modules/constants.ts
+++ src/app/modules/constants.ts
@@ -1,5 +1,74 @@
 export class Constants {
-  public static readonly api = [
-    // Place data to be displayed here
-  ]
+  public static readonly projectsList = [
+    {
+      project: 'aramex',
+      description: 'We are an international shipping service that allows you to shop from Australia, Canada, China, Cyprus, Czech Republic, Egypt, France, Georgia, Germany, Greece, Hong Kong, India, Indonesia, Italy, Japan, Jordan, Lebanon, Malaysia, New Zealand, Pakistan, Singapore, South Korea, Spain, Sri Lanka, Switzerland, Thailand, Turkey, United Arab Emirates, UK, and USA and then we deliver it to you at rates you will simply love. That means more online shopping options, hassle-free.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'aperol',
+      description: 'Aperol is an Italian bitter apéritif made of gentian, rhubarb, and cinchona, among other ingredients. It has a vibrant orange hue. Its name comes from the French slang word for aperitif, which is apero.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'hubble stories',
+      description: 'A custom take on instagram"s stories, with added information view, as well as a grid design layout to view all stories currently listed in the application, whether it be images or videos advertising, hubble stories can handle it.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator, REST API, JSON'
+    },
+    {
+      project: 'dstv diva',
+      description: 'DSTV Diva is a promotion campaign from DSTV aimed at attracting more dstv subscribers to be aware of the tv series The Real Housewives of Johannesburg, this campaign also hopes to attract more non-dstv subscribers to subscribe to dstv and its the services that they offer.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'balwin properties',
+      description: 'Balwin Properties is a listed JSE company with more than two decades’ experience in the South African property industry. Since 1996, Balwin has been designing, building, marketing and selling secure, affordable, high-quality, environmentally-friendly sectional title residential apartments. From our head office in Johannesburg, Gauteng and our regional offices in Stellenbosch, Western Cape and Durban, KwaZulu-Natal, our experienced team of professionals oversee all aspects of the development process to ensure the high standards of quality Balwin has become renowned for are maintained.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'multiplex',
+      description: 'Multiplex Cinema are a chain of multiplex cinemas based in Ukraine. The first cinema in the chain was the City Centre Cinema in Mykolaiv, which was established in 2003. Cinemas in other cities and towns were then added to the newly formed Multiplex Group over the next few years.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator, REST API, JSON'
+    },
+    {
+      project: 'shopping mall',
+      description: 'Shopping Mall is a Ukraine based fashion store, housing all the latest in fashion trends and designs at affordable prices. With over 60 different design trends, you are sure to find the trend you looking for, whether you male or female.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator, REST API, JSON'
+    },
+    {
+      project: 'dstv elevator of surprise',
+      description: 'DSTV Elevator of surprise is a promotion campaign from DSTV aimed at attracting more dstv subscribers to be aware of the all the latest trends of movie and sports views, this campaign also hopes to attract more non-dstv subscribers to subscribe to dstv and its the services that they offer.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'justin bieber purpose',
+      description: 'Purpose is the fourth studio album by Canadian singer Justin Bieber. It was released on November 13, 2015 by Def Jam Recordings and School Boy Records.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'mcDonalds excuses',
+      description: 'McDonald’s South Africa recently dropped the price of the Big Mac Medium Meal to only R39.90. This campaign is about all the random, smart and ridiculous excuses people can use to get their hands on this incredible deal. Because at only R39.90, any excuse is the perfect excuse to get a Big Mac Medium Meal.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'bizcash',
+      description: 'Bizcash has developed its solutions at the forefront of financial technology, designed to serve business leaders of the 21st century with the speed and flexibility that their businesses of today – and tomorrow – demand. Our mission is to build lifelong partnerships with our clients on their journey to growth and prosperity.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator'
+    },
+    {
+      project: 'memory game',
+      description: 'A simple memory game with extra functionality used to showcase front-end vanilla javascript development capabilities',
+      tech_stack: 'HTML/HTML5, CSS/CSS3, JavaScript ES5, Adobe Photoshop'
+    },
+    {
+      project: 'visa rush',
+      description: 'Visa Rushcard wanted a series of fun vibrant videos that spoke to their demographic when showing the many uses for the VisaRush card. This spot included a young woman going through her day, with Visa Rushcard helping her make fun purchases.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, Adobe Photoshop/Illustrator, REST API, JSON'
+    },
+    {
+      project: 'hubble maps',
+      description: 'Hubble Africa has advertising screens inside uber vehicles which houses content aimed to advertise to the uber passengers who interacts with the content, the interactions gets tracked as the user engages. Hubble Maps is an online web application developed to showcase these interactions for specific Hubble Africa clients on selected days, or on a range of selected days.',
+      tech_stack: 'React, React Hooks, JSX, SCSS/CSS3, ES6, REST API, JSON'
+    },
+  ];
 }
